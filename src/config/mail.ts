cd0801d6dba26f4cdd168b2e5b2ab7b82527cba1
luCAOrx <lucaorx@protonmail.com>
2021-11-02 05:34:49 -0300

    Adicionado diretório de configurações

---
+++ src/config/mail.ts
@@ -0,0 +1,8 @@
+export default {
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: process.env.SMTP_SECURE,
+  user: process.env.SMTP_USER,
+  pass: process.env.SMTP_PASS,
+  ciphers: process.env.SMTP_CIPHERS
+}
---
+++ src/config/multer.ts
@@ -0,0 +1,61 @@
+import { Request } from 'express';
+
+import multer, { FileFilterCallback } from 'multer';
+
+import path from  'path';
+import crypto from 'crypto';
+
+import multerS3 from 'multer-s3';
+import aws from 'aws-sdk';
+
+const doisMB = 2 * 1024 * 1024;
+
+const tiposArmazenados = {
+  local: multer.diskStorage({
+    destination: path.resolve(__dirname, '..', '..', 'uploads'),
+    filename(request, file: Express.MulterS3.File, callback) {
+      const hash = crypto.randomBytes(6).toString('hex');
+
+      file.key = `${hash}-${file.originalname}`;
+
+      callback(null, file.key);
+    }
+  }),
+  s3: multerS3({
+    s3: new aws.S3(),
+    bucket: 'roseestetica-upload',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key: (request, file: Express.MulterS3.File, callback) => {
+      const hash = crypto.randomBytes(6).toString('hex');
+
+      const fileName = `${hash}-${file.originalname}`;
+
+      callback(null, fileName);
+    }
+  }),
+};
+
+export default {
+  destination: path.resolve(__dirname, '..', '..', 'uploads'),
+  storage: process.env.STORAGE_TYPE === 'local' ? tiposArmazenados.local : tiposArmazenados.s3,
+  limits: {
+    fileSize: doisMB
+  },
+
+  fileFilter(request: Request, file: Express.Multer.File, callback: FileFilterCallback) {
+    const allowedMimes = [
+      'image/jpeg',
+      'image/pjpeg',
+      'image/png',
+      'image/jpg',
+    ];
+
+    if (allowedMimes.includes(file.mimetype)) {
+      callback(null, true);
+    } else {
+      callback(null, false);
+      callback(new Error('Tipo de arquivo inválido.'));
+    }
+  }
+};
