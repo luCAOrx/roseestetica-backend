bbbfb95cdba3ffe06018d9716a975bd43bff61d8
luCAOrx <lucaorx@protonmail.com>
2021-11-04 02:39:38 -0300

    Adicionado lint e removido pool do production

--- knexfile.ts
+++ knexfile.ts
@@ -6,35 +6,35 @@ module.exports = {
     client: 'mysql',
     version: '5.7',
     connection: {
-      host : process.env.KNEX_HOST,
+      host: process.env.KNEX_HOST,
       port: process.env.KNEX_PORT,
-      user : process.env.KNEX_USER,
-      password : process.env.KNEX_PASSWORD,
-      database : process.env.KNEX_DATABASE,
+      user: process.env.KNEX_USER,
+      password: process.env.KNEX_PASSWORD,
+      database: process.env.KNEX_DATABASE,
     },
     useNullAsDefault: true,
     migrations: {
       extension: 'ts',
-      directory: './src/database/migrations'
+      directory: './src/database/migrations',
     },
     seeds: {
       extension: 'ts',
-      directory: './src/database/seeds'
+      directory: './src/database/seeds',
     },
   },
 
   test: {
     client: 'sqlite3',
     connection: {
-      filename: './src/database/test.sqlite'
+      filename: './src/database/test.sqlite',
     },
     migrations: {
       extension: 'ts',
-      directory: './src/database/migrations'
+      directory: './src/database/migrations',
     },
     seeds: {
       extension: 'ts',
-      directory: './src/database/seeds'
+      directory: './src/database/seeds',
     },
     useNullAsDefault: true,
   },
@@ -43,40 +43,36 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
-      password: 'password'
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: 'knex_migrations',
+    },
   },
 
   production: {
     client: 'mysql',
     version: '5.7',
     connection: {
-      host : process.env.RDS_HOST,
+      host: process.env.RDS_HOST,
       port: process.env.RDS_PORT,
-      user : process.env.RDS_USER,
-      password : process.env.RDS_PASSWORD,
-      database : process.env.RDS_DATABASE,
+      user: process.env.RDS_USER,
+      password: process.env.RDS_PASSWORD,
+      database: process.env.RDS_DATABASE,
     },
     useNullAsDefault: true,
     migrations: {
       extension: 'ts',
-      directory: './src/database/migrations'
+      directory: './src/database/migrations',
     },
     seeds: {
       extension: 'ts',
-      directory: './src/database/seeds'
+      directory: './src/database/seeds',
     },
-    pool: {
-      min: 1,
-      max: 1
-    }
-  }
+  },
 };
