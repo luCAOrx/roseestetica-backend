0aa2b408964e61d7ddad0b4f2f82cbdbef38e028
luCAOrx <lucaorx@protonmail.com>
2021-11-25 03:02:02 -0300

    Adicionado novos middlewares globais

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import 'express-async-errors';
 import path from 'path';
 
 import errorHandler from './errors/handler';
+import authMiddleware from './middlewares/authMidleware';
 import routes from './routes';
 
 const app = express();
@@ -14,6 +15,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+
+app.use('/login');
+app.use(authMiddleware);
 
 app.use((request, response, next) => {
   next(response.status(404).json({ erro: 'Página não encontrada' }));
